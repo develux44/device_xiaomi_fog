845099d56c9fa8213b4145815b770e103b8f6720
develux44 <erroor234@gmail.com>
2024-04-22 17:16:08 +0500

    first commit

---
+++ BoardConfig.mk
@@ -0,0 +1,82 @@
+# Copyright (C) 2023 Paranoid Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+DEVICE_PATH := device/xiaomi/fog
+
+# Board Info
+TARGET_BOARD_INFO_FILE := $(DEVICE_PATH)/board-info.txt
+
+# HIDL
+DEVICE_FRAMEWORK_COMPATIBILITY_MATRIX_FILE += $(DEVICE_PATH)/configs/hidl/xiaomi_framework_compatibility_matrix.xml
+DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/configs/hidl/xiaomi_manifest.xml
+DEVICE_MATRIX_FILE := $(DEVICE_PATH)/configs/hidl/compatibility_matrix.xml
+
+ODM_MANIFEST_SKUS += c3qn
+ODM_MANIFEST_C3QN_FILES := $(DEVICE_PATH)/configs/hidl/manifest_c3qn.xml
+
+# Init
+TARGET_INIT_VENDOR_LIB := //$(DEVICE_PATH):libinit_fog
+TARGET_RECOVERY_DEVICE_MODULES := libinit_fog
+
+# Kernel
+BOARD_MKBOOTIMG_ARGS += --dtb $(DEVICE_PATH)-kernel/dtb.img
+BOARD_PREBUILT_DTBOIMAGE := $(DEVICE_PATH)-kernel/dtbo.img
+
+# OTA assert
+TARGET_OTA_ASSERT_DEVICE := fog,rain,wind
+
+# Screen density
+TARGET_SCREEN_DENSITY := 320
+
+HWUI_COMPILE_FOR_PERF := true
+
+# Kernel
+BOARD_KERNEL_BASE        := 0x00000000
+BOARD_KERNEL_IMAGE_NAME  := Image.gz
+BOARD_KERNEL_OFFSET      := 0x00008000
+BOARD_KERNEL_PAGESIZE    := 4096
+BOARD_RAMDISK_OFFSET     := 0x01000000
+BOARD_TAGS_OFFSET        := 0x00000100
+
+BOARD_BOOT_HEADER_VERSION := 3
+BOARD_MKBOOTIMG_ARGS += --header_version $(BOARD_BOOT_HEADER_VERSION)
+BOARD_MKBOOTIMG_ARGS += --dtb $(TARGET_PREBUILT_DTB)
+
+BOARD_PREBUILT_DTBOIMAGE := device/xiaomi/fog-kernel/dtbo.img
+TARGET_PREBUILT_DTB := device/xiaomi/fog-kernel/dtb.img
+
+BOARD_KERNEL_CMDLINE += \
+    androidboot.init_fatal_reboot_target=recovery \
+    androidboot.memcg=1 \
+    androidboot.usbcontroller=4e00000.dwc3 \
+    loop.max_part=7 \
+    lpm_levels.sleep_disabled=1 \
+    msm_rtb.filter=0x237 \
+    service_locator.enable=1 \
+    swiotlb=2048
+
+TARGET_KERNEL_ARCH := arm64
+TARGET_KERNEL_CONFIG := vendor/fog-perf_defconfig
+TARGET_KERNEL_HEADERS := kernel/xiaomi/fog
+TARGET_KERNEL_SOURCE := kernel/xiaomi/fog
+TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_LINUX_KERNEL_VERSION := 4.19
+
+BOARD_USES_QCOM_HARDWARE := true
+
+# Inherit from the proprietary version
+include vendor/xiaomi/fog/BoardConfigVendor.mk
+
+# Inherit from sm6225-common
+include device/xiaomi/sm6225-common/BoardConfigCommon.mk
---
+++ device.mk
@@ -0,0 +1,159 @@
+# Copyright (C) 2023 Paranoid Android
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+# http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# AAPT
+PRODUCT_AAPT_CONFIG := normal
+PRODUCT_AAPT_PREF_CONFIG := xhdpi
+
+PRODUCT_PACKAGES += \
+    sensors.xiaomi \
+    android.hardware.bluetooth.audio-impl \
+    android.hardware.bluetooth.audio-impl-qti
+
+
+VENDOR_PROPERTY_OVERRIDES += \
+    vendor.display.enable_async_powermode=0 \
+    debug.renderengine.backend=skiaglthreaded \
+    ro.hwui.render_ahead=20
+
+PRODUCT_PRODUCT_PROPERTIES += \
+    persist.dbg.volte_avail_ovr=1 \
+    persist.dbg.wfc_avail_ovr=1 \
+    persist.dbg.vt_avail_ovr=1
+
+# GFX
+PRODUCT_VENDOR_PROPERTIES += \
+    ro.config.avoid_gfx_accel=true
+
+PRODUCT_PACKAGES += \
+    android.hardware.gnss-V2-ndk.vendor \
+    vendor.qti.gnss@4.0 \
+    vendor.qti.gnss@4.0-service \
+    vendor.qti.hardware.iop@2.0 \
+    vendor.qti.hardware.iop@2.0-service \
+    vendor.qti.hardware.iop@2.0-impl
+
+# API Level
+PRODUCT_SHIPPING_API_LEVEL := 30
+
+# AB
+TARGET_IS_VAB := true
+PRODUCT_VIRTUAL_AB_OTA := true
+
+# Audio
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/audio/,$(TARGET_COPY_OUT_VENDOR)/etc)
+
+PRODUCT_COPY_FILES += \
+    $(LOCAL_PATH)/configs/audio/mixer_paths_bengal_idp.xml:$(TARGET_COPY_OUT_VENDOR)/etc/mixer_paths_idp_india.xml
+
+PRODUCT_VENDOR_PROPERTIES += \
+    ro.vendor.audio.policy.engine.odm=true
+
+# Boot animation
+TARGET_SCREEN_HEIGHT := 1650
+TARGET_SCREEN_WIDTH := 720
+
+# Camera
+PRODUCT_PACKAGES += \
+    libpiex_shim
+
+# Init script
+PRODUCT_PACKAGES += \
+    init.device.rc
+
+# Fingerprint
+PRODUCT_PACKAGES += \
+    android.hardware.biometrics.fingerprint@2.1 \
+    android.hardware.biometrics.fingerprint@2.1-service
+
+# FUSE passthrough
+PRODUCT_SYSTEM_PROPERTIES += \
+    persist.sys.fuse.passthrough.enable=true
+
+# Kernel
+TARGET_PREBUILT_KERNEL := $(LOCAL_PATH)-kernel/kernel
+
+PRODUCT_COPY_FILES += \
+    $(TARGET_PREBUILT_KERNEL):kernel \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)-kernel/vendor-modules,$(TARGET_COPY_OUT_VENDOR)/lib/modules)
+
+PRODUCT_VENDOR_KERNEL_HEADERS += $(LOCAL_PATH)-kernel/kernel-headers
+
+# Media
+PRODUCT_ODM_PROPERTIES += \
+    media.settings.xml=/vendor/etc/media_profiles_khaje.xml
+
+# NFC
+TARGET_COMMON_QTI_COMPONENTS += \
+    nfc
+
+TARGET_COMMON_QTI_COMPONENTS += \
+    adreno \
+    alarm \
+    audio \
+    av \
+    bt \
+    display \
+    gps \
+    init \
+    media \
+    nfc \
+    overlay \
+    perf \
+    telephony \
+    vibrator \
+    wfd \
+    wlan 
+
+TARGET_NFC_SKU :=  c3qn
+
+PRODUCT_PACKAGES += \
+    libchrome.vendor \
+    SecureElement
+
+PRODUCT_SYSTEM_PROPERTIES += \
+    ro.nfc.port=I2C \
+    ro.hardware.nfc_nci=pn8x
+
+PRODUCT_COPY_FILES += \
+    $(call find-copy-subdir-files,*,$(LOCAL_PATH)/configs/nfc/,$(TARGET_COPY_OUT_VENDOR)/etc)
+
+# Overlays
+PRODUCT_PACKAGES += \
+    AOSPAFogFrameworksOverlay \
+    FogFrameworksOverlay \
+    FogPowerFrameworksOverlay \
+    FogSettingsOverlay \
+    FogSystemUIOverlay \
+    FogWifiOverlay \
+    SettingsProvider220333QAGOverlay \
+    SettingsProvider220333QLOverlay \
+    SettingsProvider220333QBIOverlay \
+    SettingsProvider220333QNYOverlay
+
+# Soong namespaces
+PRODUCT_SOONG_NAMESPACES += \
+    $(LOCAL_PATH)
+
+# VNDK
+PRODUCT_COPY_FILES += \
+    prebuilts/vndk/v32/arm64/arch-arm-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib/libhidlbase-v32.so \
+    prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libhidlbase-v32.so
+
+# Inherit from vendor blobs
+$(call inherit-product, vendor/xiaomi/fog/fog-vendor.mk)
+
+# Inherit from sm6225-common
+$(call inherit-product, device/xiaomi/sm6225-common/common.mk)
