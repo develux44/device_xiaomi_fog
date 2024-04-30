081fa76f26b572625aa89124902de09a29157ab7
develux44 <erroor234@gmail.com>
2024-04-30 19:34:16 +0500

    some gnss things

--- device.mk
+++ device.mk
@@ -12,6 +12,9 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+BOARD_VENDOR_QCOM_GPS_LOC_API_HARDWARE := default
+LOC_HIDL_VERSION := 4.0
+
 # AAPT
 PRODUCT_AAPT_CONFIG := normal
 PRODUCT_AAPT_PREF_CONFIG := xhdpi
