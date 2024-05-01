9b394c40836da766e36d5bf5a83fbcc9b2427a81
develux44 <erroor234@gmail.com>
2024-05-01 20:55:26 +0500

    nfc stuff

--- device.mk
+++ device.mk
@@ -120,7 +120,8 @@ TARGET_NFC_SKU :=  c3qn
 
 PRODUCT_PACKAGES += \
     libchrome.vendor \
-    SecureElement
+    SecureElement \
+    vendor.nxp.hardware.nfc@2.0-service
 
 PRODUCT_SYSTEM_PROPERTIES += \
     ro.nfc.port=I2C \
@@ -150,6 +151,9 @@ PRODUCT_SOONG_NAMESPACES += \
 PRODUCT_COPY_FILES += \
     prebuilts/vndk/v32/arm64/arch-arm-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib/libhidlbase-v32.so \
     prebuilts/vndk/v32/arm64/arch-arm64-armv8-a/shared/vndk-sp/libhidlbase.so:$(TARGET_COPY_OUT_VENDOR)/lib64/libhidlbase-v32.so
+
+# Health
+TARGET_USE_HIDL_QTI_HEALTH := true
 
 # Inherit from vendor blobs
 $(call inherit-product, vendor/xiaomi/fog/fog-vendor.mk)
